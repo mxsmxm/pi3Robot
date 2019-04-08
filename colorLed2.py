d99713918fa099bdf9747eacb30678cdaa284ea8
mxsmxm <562301153@qq.com>
2019-04-08 12:42:26 +0800

    led 2

--- colorLed2.py
+++ colorLed2.py
@@ -11,8 +11,8 @@ pixels=[]
 for x in range(0,8):
     pixels.append([0,0,0])
 
-while 1:
-    for i in range(0,N):
-        pixels[i]=[randint(0,255),randint(0,255),randint(0,255)]
-        ws2812.write2812(spi,pixels)
-    time.sleep(0.1)
+
+for i in range(0,N):
+    pixels[i]=[randint(0,255),randint(0,255),randint(0,255)]
+    ws2812.write2812(spi,pixels)
+time.sleep(0.1)
