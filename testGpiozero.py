1218a8e28cdf9db3a7712b3f682908d2937ff920
mxsmxm <562301153@qq.com>
2019-04-09 12:31:54 +0800

    gpiozero

--- testGpiozero.py
+++ testGpiozero.py
@@ -2,11 +2,11 @@ from gpiozero import LED,LineSensor
 from time import sleep
 
 red = LED(17)
-lsensor=LineSensor(23)
-rsensor=LineSensor(16)
+lsensor=LineSensor(23,pull_up=True)
+rsensor=LineSensor(16,pull_up=True)
 
 lsensor.when_line= lambda: red.on()
-rsensor.when_line= lambda:red.off()
+lsensor.when_no_line= lambda:red.off()
 
 while True:
     sleep(0.02)
